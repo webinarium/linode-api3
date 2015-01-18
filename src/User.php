c3e882ec7c79b3ea33f438643e4647764b9e9634
Artem Rodygin <artem.rodygin@gmail.com>
2015-01-19 01:08:10 +1300

    user.* tests

--- src/User.php
+++ src/User.php
@@ -26,22 +26,22 @@ class User extends BaseLinodeApi
      * Authenticates a Linode Manager user against their username, password, and two-factor token (when enabled), and then returns a new API key, which can be used until it expires.
      * The number of active keys is limited to 20.
      *
-     * @param   string  $label    [optional] An optional label for this key.
-     * @param   integer $expires  [optional] Number of hours the key will remain valid, between 0 and 8760. 0 means no expiration. Defaults to 168.
      * @param   string  $username [required]
      * @param   string  $password [required]
      * @param   string  $token    [optional] Required when two-factor authentication is enabled.
+     * @param   integer $expires  [optional] Number of hours the key will remain valid, between 0 and 8760. 0 means no expiration. Defaults to 168.
+     * @param   string  $label    [optional] An optional label for this key.
      *
      * @return  array
      */
-    public function getApiKey($label = null, $expires = null, $username = null, $password = null, $token = null)
+    public function getApiKey($username, $password, $token = null, $expires = null, $label = null)
     {
         return $this->call('user.getapikey', array(
-            'label'    => $label,
-            'expires'  => $expires,
             'username' => $username,
             'password' => $password,
             'token'    => $token,
+            'expires'  => $expires,
+            'label'    => $label,
         ));
     }
 }
---
+++ tests/UserTest.php
@@ -0,0 +1,41 @@
+<?php
+
+//----------------------------------------------------------------------
+//
+//  Copyright (C) 2015 Artem Rodygin
+//
+//  This file is part of Linode API Client Library for PHP.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with the library. If not, see <http://www.gnu.org/licenses/>.
+//
+//----------------------------------------------------------------------
+
+require_once(__DIR__ . '/../vendor/autoload.php');
+
+use Linode\User;
+
+class UserTest extends \PHPUnit_Framework_TestCase
+{
+    /** @var string */
+    private $key;
+
+    /** @var User */
+    private $api = null;
+
+    protected function setUp()
+    {
+        $this->key = uniqid();
+        $this->api = new User($this->key, true);
+    }
+
+    public function testGetApiKey()
+    {
+        $username = uniqid();
+        $password = uniqid();
+
+        $expected = "api_key={$this->key}&api_action=user.getapikey&username={$username}&password={$password}";
+        $query = $this->api->getApiKey($username, $password);
+        $this->assertEquals($expected, $query);
+    }
+}
